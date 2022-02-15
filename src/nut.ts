ffba205f161976fefae266cf39ba7a34929934f5
Benoit-Welsch <benoitwelsch@protonmail.com>
2022-02-15 22:13:52 +0100

    Feat : Regex Test

--- src/nut.ts
+++ src/nut.ts
@@ -1,29 +1,31 @@
-import { promisify } from "util"
-import { exec } from "child_process"
-import parseDot from '@lv00/dot-parser'
+import { promisify } from 'util';
+import { exec } from 'child_process';
+import { ipRegex, nameRegex, portRegex } from './regex';
 
-const run = promisify(exec)
+const run = promisify(exec);
 
 export default class Nut {
-  readonly ip: string
-  readonly port: string
-  readonly name: string
-  readonly CMD: string
+  readonly ip: string;
+  readonly port: string;
+  readonly name: string;
+  readonly CMD: string;
 
-  constructor({ NUT_UPS_NAME = "ups", NUT_IP = "127.0.0.1", NUT_PORT = "3493" }) {
+  constructor({ NUT_UPS_NAME = 'ups', NUT_IP = '127.0.0.1', NUT_PORT = '3493' }) {
+    if (!!NUT_IP.search(ipRegex)) throw new Error('Invalid Ip');
+    if (!!NUT_PORT.search(portRegex)) throw new Error('Invalid Port');
+    if (!!NUT_UPS_NAME.search(nameRegex)) throw new Error('Invalid Name');
     this.ip = NUT_IP;
     this.name = NUT_UPS_NAME;
     this.port = NUT_PORT;
-    this.CMD = `upsc ${this.name}@${this.ip}:${this.port}`
-    console.log(this.CMD)
+    this.CMD = `upsc ${this.name}@${this.ip}:${this.port}`;
+    console.log(this.CMD);
   }
 
   async read() {
-    const { stderr, stdout } = await run(this.CMD)
-    if (stderr) throw new Error(stderr)
-    const res = parseDot(stdout)
-    if (!res) throw new Error('Unable to parse data from Nut')
-    return res
+    const { stderr, stdout } = await run(this.CMD);
+    if (stderr) throw new Error(stderr);
+    const res = stdout;
+    if (!res) throw new Error('Unable to parse data from Nut');
+    return res;
   }
 }
-
