807c645a05a8808e91f0ce9986a3c80312d09df2
Benoit-Welsch <benoitwelsch@protonmail.com>
2022-02-17 11:20:30 +0100

    Feat : env.local ✨

--- src/var.ts
+++ src/var.ts
@@ -1,4 +1,4 @@
-import { config } from 'dotenv';
+import { config } from 'dotenv-flow';
 
 export const ipRegex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/g;
 export const portRegex = /^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$/g;
