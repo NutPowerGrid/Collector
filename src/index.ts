fad1f1084c5a8cc7018f03bcaa95587b7fd55f4f
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-11-15 13:34:09 +0000

    Feat : Plugin info on startup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { load } from './plugins';
 import Nut from './nut';
 import logger from './logger';
+import { getMemoryUsage } from 'unit';
 
 const init = async () => {
   const loaded = await load();
+  const message = loaded.map((plugin) => '\n • ' + plugin.constructor.name).join('');
+  logger.log({ level: 'info', message: 'Power monitor enable' });
+  logger.log({ level: 'info', message: `Loaded plugins (${loaded.length}):` + message });
   if (loaded.length === 0) {
     logger.log({ level: 'warn', message: 'No plugin loaded -> Exiting' });
     process.exit(1);
